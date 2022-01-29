b6c9a84851434c22d56c95826b1c184648cfd8a4
Juho Nykänen <juho.nykanen@hotmail.fi>
2022-01-29 20:47:54 +0200

    refactor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-from utils import get_delivery_fee
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -28,3 +29,27 @@ def get_status():
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
+
+API_URL = 'https://apim-kotipizza-ecom-prod.azure-api.net/webshop/v1/restaurants/nearby?type=DELIVERY'
+COORDS = '&coordinates='
+DEFAULT = '62.6153386,29.7500527'
+
+
+def read_data_json(coords):
+    print(API_URL + COORDS + coords)
+    req = requests.get(API_URL + COORDS + coords)
+    return json.loads(req.content)
+
+
+def get_delivery_fee(coords=DEFAULT) -> str:
+    data = read_data_json(coords)
+
+    if data and len(data) > 0:
+        # TODO useamman ravintolan händläys mikäli niitä löytyy, tällä hetkellä otetaan ensimmäinen
+        data = data[0]
+        if data['openForDeliveryStatus'] == "CLOSED":
+            return "Suljettu!"
+        if "dynamicDeliveryFee" in data:
+            return str(data["dynamicDeliveryFee"]) + " €"
+        return "Tietoa ei saatavilla."
+    return "Tietoa ei saatavilla."
